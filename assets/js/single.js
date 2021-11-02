75898ab89ba27c075238b5fb85b7c63319641ced
hpurring <hilarypurrington@gmail.com>
2021-11-02 17:33:08 -0400

    more than 30 pages

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,6 @@
+var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     console.log(repo);
@@ -9,14 +11,18 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
+
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         }
         else {
+            console.log(response);
             alert("There was a problem with your request!");
         }
     });
-
-}
+};
 
 var displayIssues = function(issues) {
     if (issues.length === 0) {
@@ -51,10 +57,20 @@ var displayIssues = function(issues) {
         issueEl.appendChild(typeEl);
 
         issueContainerEl.appendChild(issueEl);
-}
-
-    
-
+    }
 };
 
-getRepoIssues("hpurring/js-quiz");
+var displayWarning = function(repo) {
+    //add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See more issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    //append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
+
+getRepoIssues("facebook/react");
